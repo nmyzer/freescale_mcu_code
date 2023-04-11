5a98dc58c68f6e7b02b3c1ad841283d8d130c6c5
nmyzer <158168697@qq.com>
2023-04-11 11:13:31 +0800

    feature1 add new module

--- SourceCode/main.c
+++ SourceCode/main.c
@@ -9,6 +9,15 @@ int add(int a, int b)
     return a + b;
 }
 
+/*
+时间：2023年4月11日11:11:45
+概述：1.添加sub模块
+*/
+int sub(int a, int b)
+{
+    return a - b;
+}
+
 int main(void)
 {
     printf("Hello Github!\r\n");
